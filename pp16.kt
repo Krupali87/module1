6e1dd93c71ccbf1b17771098fb2550671a548a10
Krupali87 <119691073+Krupali87@users.noreply.github.com>
2022-12-14 11:51:34 +0530

    Add files via upload

---
+++ pp16.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    var num = 54.12
+    if (num<0.0)
+    {
+        println("$num is negative")
+    }
+    else if (num>0.0)
+    {
+    println("$num is positive")
+    }
+    else
+    {
+        println("$num is 0")
+    }
+}
